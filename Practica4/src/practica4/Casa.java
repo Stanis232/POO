e3b78b054c7e9b990ef3b14c0dd629c86da729f0
stanis <st.yaruchyk@gmail.com>
2025-10-17 20:34:38 +0200

    prueba para ver si van bien los metodos de añadir sensores

--- Practica4/src/practica4/Casa.java
+++ Practica4/src/practica4/Casa.java
@@ -15,4 +15,55 @@ public class Casa {
     
     private Evento[] evento = new Evento[10];
     private int contadorSensores,contadorEventos;
+
+    public Casa() {
+        this.sensoresActivados = false;
+        this.contadorSensores = 0;
+        this.contadorEventos = 0;
+    }
+    
+    public boolean añadirSensor(int id, String ip){
+        if(this.contadorSensores==10){
+            return false;
+        }
+        Sensor sen = new Sensor(id, ip);
+         for(int i=0; i<this.contadorSensores; i++){
+           //habia hecho esto sen.equals(this.sensores) sin la i y era como comparar un manzana con una caja de manzana
+             if (sen.equals(this.sensores[i])) {
+                 
+                 return false;
+             
+             
+             }
+        }
+         
+         this.sensores[this.contadorSensores]= sen;
+        this.contadorSensores++;
+        return true;
+    }
+     public boolean añadirSensor(int id){
+        if(this.contadorSensores==10){
+            return false;
+        }
+        Sensor sen = new Sensor(id);
+         for(int i=0; i<this.contadorSensores; i++){
+           //habia hecho esto sen.equals(this.sensores) sin la i y era como comparar un manzana con una caja de manzana
+             if (sen.equals(this.sensores[i])) {
+                 
+                 return false;
+             
+             
+             }
+        }
+         
+         this.sensores[this.contadorSensores]= sen;
+        this.contadorSensores++;
+        return true;
+    }
+    
+
+
 }
+    
+    
+
